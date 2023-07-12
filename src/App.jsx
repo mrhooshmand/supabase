8abbadc0ec0fbdd44ff4c68dc7025c7e80ac4da5
hushmand <hushmand@safarbooking.com>
2023-07-12 15:43:51 +0330

    add city

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,37 @@ import { createClient } from "@supabase/supabase-js";
 const supabase = createClient("https://cdfrzipdmoxiyqnpnquv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkZnJ6aXBkbW94aXlxbnBucXV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkwNzU4MjYsImV4cCI6MjAwNDY1MTgyNn0.Z-bpN4tSmre9xKXdtZZQ6CiynjRrn_OeQt-YP5G--po");
 
 function App() {
+  const [city, setCity] = useState([]);
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
     getCountries();
+    getCity();
   }, []);
 
   async function getCountries() {
     const { data } = await supabase.from("city").select();
+    setCity(data);
+  }
+  async function getCity() {
+    const { data } = await supabase.from("countries").select();
     setCountries(data);
   }
 
   return (
+    <>
+    <ul>
+      {city.map((city) => (
+        <li key={city.name}>{city.name}</li>
+      ))}
+    </ul>
     <ul>
       {countries.map((country) => (
         <li key={country.name}>{country.name}</li>
       ))}
     </ul>
+    
+    </>
   );
 }
 
